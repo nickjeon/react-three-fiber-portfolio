24fb74a466bce333a0530817198e7dbe6702b7f0
nickjeon <nicholaskjeon@gmail.com>
2022-10-26 13:41:54 -0400

    updated App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { CycleRaycast, softShadows } from '@react-three/drei';
 
-function App() {
+import Stage from './components/Stage';
+import Stair from './components/Stair';
+
+export default function App() {
+  const [{ objects, cycle }, set] = useState({ objects: [], cycle: 0 });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* CycleRaycast's status data can now be turned into informative HTML */}
+      <div className="status">
+        {objects.map((_, i) => (<div key={i} className="dot" style={{ background: i === cycle ? '#70ffd0' : '#ccc' }} />)) /* prettier-ignore */}
+        {objects.length ? <div className="name" style={{ left: cycle * 14, padding: 2 }} children={objects[cycle].object.name} /> : null}
+      </div>
+      <Canvas shadows dpr={1.5} camera={{ position: [-10, 10, 5], fov: 50 }}>
+        <Stage />
+        {Array.from({ length: 12 }, (_, i) => (
+          <Stair
+            key={i}
+            name={'stair-' + (i + 1)}
+            rotation={[-Math.PI / 2, 0, i / Math.PI / 2]}
+            position={[2 - Math.sin(i / 5) * 5, i * 0.5, 2 - Math.cos(i / 5) * 5]}
+          />
+        ))}
+        {/* This component cycles through the raycast intersections, combine it with event.stopPropagation! */}
+        <CycleRaycast onChanged={(objects, cycle) => set({ objects, cycle })} />
+      </Canvas>
+    </>
   );
 }
 
-export default App;
+// Percentage closer soft shadows, normally *very* expensive
+softShadows();
